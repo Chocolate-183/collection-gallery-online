7df712bdbd4979eb3721d07f6e85bb74ddbc830d
auer0955 <auer0955@16910924--A955.local>
2026-09-13 20:38:32 +0800

    feat(filter): replace length filters 5-8+ with 5字+

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -134,10 +134,8 @@ test('Filter Engine - Kana Matching, Query, Length & Latest10 Sorting', () => {
 
   assert.equal(filterByLength(lengthRecords, LENGTH_TABS.ALL).length, 9);
   assert.equal(filterByLength(lengthRecords, LENGTH_TABS.ONE)[0].id, '1');
-  assert.equal(filterByLength(lengthRecords, LENGTH_TABS.FIVE)[0].id, '5');
-  assert.equal(filterByLength(lengthRecords, LENGTH_TABS.SIX)[0].id, '6');
-  assert.equal(filterByLength(lengthRecords, LENGTH_TABS.SEVEN)[0].id, '7');
-  assert.equal(filterByLength(lengthRecords, LENGTH_TABS.EIGHT_PLUS).length, 2);
+  assert.equal(filterByLength(lengthRecords, LENGTH_TABS.FIVE_PLUS).length, 5);
+  assert.equal(filterByLength(lengthRecords, LENGTH_TABS.FIVE_PLUS)[0].id, '5');
 });
 
 test('Config & Endpoint URL Builders', () => {
